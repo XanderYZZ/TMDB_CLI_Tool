7612b162dfc9caa1295d3b4861c5ef1bd9b9e3f8
Xander <87785817+XanderYZZ@users.noreply.github.com>
2025-12-22 20:26:58 -0700

    Update.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,82 @@
 #include <cpr/cpr.h>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <unordered_map>
 
-int main(int argc, char** argv) {
-    std::cout << "Program started!" << std::endl;
-    std::cout.flush();  // Force immediate output
+const std::unordered_map<std::string, std::string> instruction_to_url = {
+    {"playing", "https://api.themoviedb.org/3/movie/now_playing",},
+    {"popular", "https://api.themoviedb.org/3/movie/popular",},
+    {"top", "https://api.themoviedb.org/3/movie/top_rated",},
+    {"upcoming", "https://api.themoviedb.org/3/movie/upcoming",},
+};
+
+std::string api_key;
+
+void GetAPIKey() {
+    std::ifstream file(".env");
+
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open the .env file!");
+    } else {
+        std::string line;
+        std::getline(file, line);
+        bool passed_equals = false;
+
+        for (const char &c : line) {
+            if (c == '=') {
+                passed_equals = true;
+            } else {
+                if (!passed_equals) { continue; }
+
+                api_key += c;
+            }
+        }
+    }
+}
+
+void SendRequest(const std::string &command) {
+    if (!instruction_to_url.count(command)) {
+        throw std::runtime_error("Invalid command! The options are: playing, popular, top, and upcoming.");
+    }
 
     cpr::Response r = cpr::Get(
-        cpr::Url{"https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"},
-        cpr::Header{{"Accept", "application/json"}}
+        cpr::Url{instruction_to_url.at(command)},
+        cpr::Parameters{
+            {"api_key", api_key},
+            {"language", "en-US"},
+            {"page", "1"}
+        },
+        cpr::Header{{"accept", "application/json"}, {"Accept-Encoding", "identity"}}
     );
 
-    std::cout << "Status: " << r.status_code << "\n";
-    std::cout << "Body length: " << r.text.length() << "\n";
-    std::cout << r.text << "\n";
+    using json = nlohmann::json;
+
+    json results = json::parse(r.text)["results"];
+    int i = 1;
+
+    for (auto it = results.begin(); it != results.end(); it++) {
+        auto val = it.value();
+
+        std::cout << "#" << i << ":\n" << "Original Title: " << val["original_title"] << "\n" 
+        << "Title: " << val["title"] << "\n"
+        << "Overview: " << val["overview"] << "\n"
+        << "Release Date: " << val["release_date"] << "\n"
+        << "Vote average: " << val["vote_average"] << "/10" << " from a total of " << val["vote_count"] << " votes.\n"
+        << "Maturity rating: This move is for" << (val["adult"] ? " adults\n" : " people of all ages.\n")
+        << "\n\n";
+
+        i++;
+    }
+}
+
+int main(int argc, char** argv) {
+    GetAPIKey();
+    
+    int i = 1;
+
+    while (i < argc) {
+        std::string command = argv[i];
+        SendRequest(command);
+        i++;
+    }
 }
